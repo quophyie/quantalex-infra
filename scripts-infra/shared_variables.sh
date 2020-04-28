e9f321c990f4e0c6c610784c7617e385851743cc
quophyie <quophyie@yahoo.com>
2020-04-28 12:44:36 +0100

    (feat): add INFRA_SCRIPTS_ROOT env variables

--- scripts-infra/shared_variables.sh
+++ scripts-infra/shared_variables.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 KONG_DOCKER_COMPOSE_SCRIPTS_ROOT=`pwd`/compose
+INFRA_SCRIPTS_ROOT=`pwd`
 CONFLUENT_PLATFORM_ALL_IN_ONE_DIR=`pwf`/../../confluent-platform/cp-all-in-one/cp-all-in-one
 QUANTAL_MS_DOCKER_COMPOSE_SCRIPTS_ROOT=`pwd`/../../
 declare -a QUANTAL_MS_DOCKER_COMPOSE_DIRS=("${QUANTAL_MS_DOCKER_COMPOSE_SCRIPTS_ROOT}quantalex-users"
