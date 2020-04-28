87b7b09f5a9ea1d3f974b8de4cfdc9787216673e
quophyie <quophyie@yahoo.com>
2020-04-28 19:43:47 +0100

    (refactor): source confluent env variables

--- scripts-infra/shared_variables.sh
+++ scripts-infra/shared_variables.sh
@@ -3,6 +3,10 @@
 KONG_DOCKER_COMPOSE_SCRIPTS_ROOT=`pwd`/compose
 INFRA_SCRIPTS_ROOT=`pwd`
 CONFLUENT_PLATFORM_ALL_IN_ONE_DIR=`pwf`/../../confluent-platform/cp-all-in-one/cp-all-in-one
+
+echo "sourcing ${CONFLUENT_PLATFORM_ALL_IN_ONE_DIR}/.env"
+source ${CONFLUENT_PLATFORM_ALL_IN_ONE_DIR}/.env
+
 QUANTAL_MS_DOCKER_COMPOSE_SCRIPTS_ROOT=`pwd`/../../
 declare -a QUANTAL_MS_DOCKER_COMPOSE_DIRS=("${QUANTAL_MS_DOCKER_COMPOSE_SCRIPTS_ROOT}quantalex-users"
                 "${QUANTAL_MS_DOCKER_COMPOSE_SCRIPTS_ROOT}quantal-auth"
