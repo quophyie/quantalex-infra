3d740d51e845204271acb80997fdc2b791cc01e9
quophyie <quophyie@yahoo.com>
2020-04-28 12:48:57 +0100

    (refactor): update script so that at end of each iteration we cd into  and also update so that confluent platform instances statuses are not displayed for every iteration

--- scripts-infra/view_running_services_docker.sh
+++ scripts-infra/view_running_services_docker.sh
@@ -12,11 +12,12 @@ check_and_source_file ~/.bash_profile
 # *** NOTE ****
 # KONG_DOCKER_COMPOSE_SCRIPTS_ROOT is defined in shared_variables.sh
 # CONFLUENT_PLATFORM_ALL_IN_ONE_DIR is defined in shared_variables.sh
+# INFRA_SCRIPTS_ROOT is defined in shared_variables.sh
 # QUANTAL_MS_DOCKER_COMPOSE_SCRIPTS_ROOT is defined in shared_variables.sh
 # QUANTAL_MS_DOCKER_COMPOSE_DIRS is defined in shared_variables.sh
 source shared_variables.sh
 
-        COMMAND="docker-compose -f ${DOCKER_COMPOSE_SCRIPTS_ROOT}/docker-compose.yml ps"
+        COMMAND="docker-compose -f ${KONG_DOCKER_COMPOSE_SCRIPTS_ROOT}/docker-compose.yml ps -f ${CONFLUENT_PLATFORM_ALL_IN_ONE_DIR}/docker-compose.yml ps"
    echo "\nRunning command $COMMAND\n"
    eval ${COMMAND}
     ## now loop through the above array
@@ -24,10 +25,10 @@ source shared_variables.sh
     do
        echo "IN $MS_DOCKER_COMPOSE_DIR"
        #COMMAND="docker-compose -f ${MS_DOCKER_COMPOSE_DIR}/build_docker_container.sh"
-       COMMAND="docker-compose -f ${MS_DOCKER_COMPOSE_DIR}/docker/compose/docker-compose.yml  -f ${CONFLUENT_PLATFORM_ALL_IN_ONE_DIR}/docker-compose.yml ps"
+       COMMAND="docker-compose -f ${MS_DOCKER_COMPOSE_DIR}/docker/compose/docker-compose.yml ps"
        echo "\nRunning command $COMMAND \n"
        eval $COMMAND
-       eval "cd ${KONG_DOCKER_COMPOSE_SCRIPTS_ROOT}"
+       eval "cd ${INFRA_SCRIPTS_ROOT}"
     done
 
 
