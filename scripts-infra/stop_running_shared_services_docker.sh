ee602101e2c372efcb1da833763cdf0a0020c5ba
quophyie <quophyie@yahoo.com>
2020-04-28 19:37:06 +0100

    (refactor): remove unused code in stop_running_shared_services_docker.sh

--- scripts-infra/stop_running_shared_services_docker.sh
+++ scripts-infra/stop_running_shared_services_docker.sh
@@ -15,35 +15,6 @@ check_and_source_file ~/.bash_profile
 
 source shared_variables.sh
 
-    ## now loop through the above array
-    for MS_DOCKER_COMPOSE_DIR in "${QUANTAL_MS_DOCKER_COMPOSE_DIRS[@]}"
-    do
-       echo "IN $MS_DOCKER_COMPOSE_DIR"
-       #COMMAND="docker-compose -f ${MS_DOCKER_COMPOSE_DIR}/build_docker_container.sh"
-       COMMAND="docker-compose -f ${MS_DOCKER_COMPOSE_DIR}/docker/compose/docker-compose.yml stop"
-
-       # Get the container ids
-       CONTAINER_IDS=(`docker-compose -f ${MS_DOCKER_COMPOSE_DIR}/docker/compose/docker-compose.yml ps -q`)
-       CONTAINERS_TO_STOP=""
-       for CONTAINER_ID in "${CONTAINER_IDS[@]}"
-        do
-
-            # Get the container name from container id
-            CONTAINER_NAME=`docker ps --filter "id=${CONTAINER_ID}" --format "{{.Names}}"`
-            CONTAINERS_TO_STOP=$(echo "${CONTAINERS_TO_STOP} ${CONTAINER_NAME},")
-
-        done
-        echo "Stopping container(s) named: ${CONTAINERS_TO_STOP}"
-
-        COMMAND="docker-compose -f ${MS_DOCKER_COMPOSE_DIR}/docker/compose/docker-compose.yml down"
-        echo "\n Running command $COMMAND\n"
-        eval ${COMMAND}
-
-
-       eval "cd ${KONG_DOCKER_COMPOSE_SCRIPTS_ROOT}"
-    done
-
-
 # Stop shared non microservice containers
 COMMAND="docker-compose -f ${KONG_DOCKER_COMPOSE_SCRIPTS_ROOT}/docker-compose.yml -f ${CONFLUENT_PLATFORM_ALL_IN_ONE_DIR}/docker-compose.yml stop"
 echo "\nRunning command $COMMAND\n"
