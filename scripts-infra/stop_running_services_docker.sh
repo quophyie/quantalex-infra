665c98d9e8e30ab89bd8297267b2a83cd00ed10a
quophyie <quophyie@yahoo.com>
2020-04-28 12:32:32 +0100

    (refactor): rename DOCKER_COMPOSE_SCRIPTS_ROOT to KONG_DOCKER_COMPOSE_SCRIPTS_ROOT and add CONFLUENT_PLATFORM_ALL_IN_ONE_DIR env variable and update scripts to use these 2 new env variables

--- scripts-infra/stop_running_services_docker.sh
+++ scripts-infra/stop_running_services_docker.sh
@@ -8,7 +8,7 @@ source ./../../scripts/docker-scripts/common_funcs.sh
 check_and_source_file ~/.bash_profile
 
 # *** NOTE ****
-# DOCKER_COMPOSE_SCRIPTS_ROOT is defined in shared_variables.sh
+# KONG_DOCKER_COMPOSE_SCRIPTS_ROOT is defined in shared_variables.sh
 # CONFLUENT_PLATFORM_ALL_IN_ONE_DIR is defined in shared_variables.sh
 # QUANTAL_MS_DOCKER_COMPOSE_SCRIPTS_ROOT is defined in shared_variables.sh
 # QUANTAL_MS_DOCKER_COMPOSE_DIRS is defined in shared_variables.sh
