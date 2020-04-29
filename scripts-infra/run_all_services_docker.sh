dc4f6993bf78e307414c42858ffc279746d46745
quophyie <quophyie@yahoo.com>
2020-04-29 23:17:39 +0100

    (refactor): remove code to start shared services and source run_shared_services_docker to start shared services
    Signed-off-by: quophyie <quophyie@yahoo.com>

--- scripts-infra/run_all_services_docker.sh
+++ scripts-infra/run_all_services_docker.sh
@@ -36,46 +36,9 @@ else
     BUILD_CONTAINER=$(echo "$2" | tr '[:upper:]' '[:lower:] | xargs')
 fi
 
-if [ "${ON_JENKINS}" ]; then
-   export PATH=$PATH:$1/bin
-   export HOST_IP=`/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1`
-else
-   export HOST_IP=`ifconfig en0 | grep inet | grep -v inet6 | cut -d' ' -f2`
-fi
 
-if [ "${HOST_IP}" ]; then
-   echo "HOST_IP: $HOST_IP"
-else
-   echo "HOST_IP has not been defined."
-   echo "Source the appropriate prepare script before running this script, or set HOST_IP to a valid IP address manually."
-   exit 1;
-fi
-
-# Remove any shutdown kafka docker containers because they may have the wrong IP address and this will stop the
-# Kafka producer scripts from working correctly after leaving the network and rejoining again later.
-EXISTING_KAFKA_DOCKER_CONATINER_ID=`docker ps -a | grep kafka |  awk '{print $1}'`
-if [ "$EXISTING_KAFKA_DOCKER_CONATINER_ID" ]; then
-   echo "Removing previous Kafka docker container with id ${EXISTING_KAFKA_DOCKER_CONATINER_ID}"
-   docker rm -f ${EXISTING_KAFKA_DOCKER_CONATINER_ID}
-fi
-
-EXISTING_ZOOKEEPER_DOCKER_CONATINER_ID=`docker ps -a | grep zook |  awk '{print $1}'`
-if [ "$EXISTING_ZOOKEEPER_DOCKER_CONATINER_ID" ]; then
-   echo "Removing previous zookeeper docker container with id ${EXISTING_ZOOKEEPER_DOCKER_CONATINER_ID}"
-   docker rm -f ${EXISTING_ZOOKEEPER_DOCKER_CONATINER_ID}
-fi
-
-if [ "${ON_JENKINS}" ]; then
-   COMMAND="REPOSITORY=${REPOSITORY} CONFLUENT_DOCKER_TAG=${CONFLUENT_DOCKER_TAG} docker-compose -f ${KONG_DOCKER_COMPOSE_SCRIPTS_ROOT}/docker-compose.yml  -f ${CONFLUENT_PLATFORM_ALL_IN_ONE_DIR}/docker-compose.yml up -d"
-   echo "Running on Jenkins $COMMAND"
-   eval ${COMMAND}
-   echo "Waiting 10 seconds for kafka / zookeeper to start before running acceptance tests on Jenkins"
-   sleep 10
-else
-   COMMAND="REPOSITORY=${REPOSITORY} CONFLUENT_DOCKER_TAG=${CONFLUENT_DOCKER_TAG} docker-compose -f ${KONG_DOCKER_COMPOSE_SCRIPTS_ROOT}/docker-compose.yml -f ${CONFLUENT_PLATFORM_ALL_IN_ONE_DIR}/docker-compose.yml up -d"
-   echo "Running on local $COMMAND"
-   eval ${COMMAND}
-fi
+# run all the shared services. send it to the background
+source ./run_shared_services_docker.sh $1 &
 
 # Build and Run Microservices
 if [ ${DEPLOY_MS} == 'true' ]; then
@@ -110,6 +73,6 @@ fi
 
 ##### THIS SHOULD ALWAYS BE THE LAST COMMAND #####
 # BRING SHARED INFRA LOGS TO THE FOREGROUND
-docker-compose -f ${KONG_DOCKER_COMPOSE_SCRIPTS_ROOT}/docker-compose.yml -f ${CONFLUENT_PLATFORM_ALL_IN_ONE_DIR}/docker-compose.yml  logs -f | tee -a ${INFRA_DOCKER_LOGS_DIR}/shared-services.log
+fg 1 && source ./show_shared_services_log.sh | tee -a ${INFRA_DOCKER_LOGS_DIR}/shared-services.log
 
 
