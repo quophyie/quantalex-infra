7adc0c1893ede57a84311d83eb0251f54b5daca4
quophyie <quophyie@yahoo.com>
2020-05-03 02:04:21 +0100

    (refactor): add code to correctly set INFRA_SCRIPTS_ROOT from any executing script

--- scripts-infra/shared_variables.sh
+++ scripts-infra/shared_variables.sh
@@ -1,7 +1,28 @@
 #!/bin/sh
 
+# if INFRA_SCRIPTS_ROOT is empty, set it
+if [ -z "${INFRA_SCRIPTS_ROOT}" ]; then
+
+#  get the correct absolute full name of the scripts-infra  directory (i.e. the directory containing this script)
+# this makes sure that no matter where this file is sourced from,
+# INFRA_SCRIPTS_ROOT will always be set to the correct absolute directory i.e. the directory containing
+# this file
+
+# see https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
+# for more info
+    SOURCE="${BASH_SOURCE[0]}"
+
+    # if this file has been symlinked the code in the while loop will resolve until
+    # we the actual directory containing this file is reached
+    while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+      DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+      SOURCE="$(readlink "$SOURCE")"
+      [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    done
+    INFRA_SCRIPTS_ROOT="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+fi
+
 KONG_DOCKER_COMPOSE_SCRIPTS_ROOT=`pwd`/../docker-kong/compose
-INFRA_SCRIPTS_ROOT=`pwd`
 CONFLUENT_PLATFORM_ALL_IN_ONE_DIR=`pwd`/../confluent-platform/cp-all-in-one
 
 echo "sourcing ${CONFLUENT_PLATFORM_ALL_IN_ONE_DIR}/.env"
